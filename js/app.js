c88029a12488d4c1427e01f26f1da51d762f533b
CoderDam <garnes.damien@gmail.com>
2017-02-15 17:25:01 +0100

    début structure JS

---
+++ js/app.js
@@ -0,0 +1,26 @@
+var app = {
+  /**
+   *  STATES
+   */
+
+
+  /**
+   *  VARIABLES
+   */
+
+
+  /**
+   *  HANDLERS
+   */
+  init: function() {
+
+  },
+
+
+
+  /**
+  *  FUNCTIONS
+  */
+};
+
+$(app.init);
---
+++ js/data.js
@@ -0,0 +1,7 @@
+var data = [
+  'images/cave-a-vin.jpg',
+  'images/epicerie-salee.jpg',
+  'images/epicerie-sucree.jpg',
+  'images/foie-gras.jpg',
+  'images/home.jpg',
+];
